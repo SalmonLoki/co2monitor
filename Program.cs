d5000f74385a6aca30717e5741b9b7dc1116b5f5
Ksenia Khromova <ksenia.khromova@corp.muranosoft.com>
2019-05-13 17:28:01 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace co2monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
